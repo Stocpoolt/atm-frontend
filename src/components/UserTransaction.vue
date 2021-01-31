127b5448472820e469a1bbe30d180167da4acaeb
Stocpoolt <stocpoolt@gmail.com>
2021-01-31 16:45:00 -0500

    Jan31 16:44

--- src/components/UserTransaction.vue
+++ src/components/UserTransaction.vue
@@ -30,7 +30,7 @@ export default {
                 username: this.username,
                 value: this.value
             }
-            axios.put("https://stocpoolt-atm-backend.herokuapp.com/transaction/", transaction_in,  {headers: {}})
+            axios.put("http://localhost:8000/transaction/", transaction_in,  {headers: {}})
                 .then((result) => {
                     alert("Transaction Correcta, Saldo Restante: " + result.data.actual_balance);
                     
